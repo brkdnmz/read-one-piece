53309edfac08073b3003f78f13d9a023164422db
Berke Dönmez <brkdnmz99@gmail.com>
2026-02-12 01:42:29 +0300

    "one piece"-ify theme switcher with a little animation

--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -1,6 +1,9 @@
-import { Moon, Sun } from "lucide-react";
+import { AnimatePresence, motion } from "motion/react";
+import React from "react";
 import { useTheme } from "./theme-provider";
 import { Button } from "./ui/button";
+import SunPiratesJollyRoger from "/sun-pirates-jolly-roger.svg";
+import MoonMoonFruit from "/moon-moon-fruit.webp";
 
 export function ThemeSwitcher() {
   const { theme, setTheme } = useTheme();
@@ -19,7 +22,24 @@ export function ThemeSwitcher() {
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
       title="Switch theme"
     >
-      {effectiveTheme === "light" ? <Sun /> : <Moon />}
+      <AnimatePresence mode="sync" initial={false}>
+        <motion.div
+          key={effectiveTheme}
+          initial={{ opacity: 0, transform: "scale(0.7)" }}
+          animate={{ opacity: 1, transform: "scale(1)" }}
+          exit={{ opacity: 0, transform: "scale(0.7)" }}
+          transition={{
+            transform: { type: "spring", stiffness: 500, damping: 20 },
+          }}
+          className="absolute"
+        >
+          {effectiveTheme === "light" ? (
+            <img src={SunPiratesJollyRoger} className="h-5" />
+          ) : (
+            <img src={MoonMoonFruit} className="h-5" />
+          )}
+        </motion.div>
+      </AnimatePresence>
     </Button>
   );
 }
